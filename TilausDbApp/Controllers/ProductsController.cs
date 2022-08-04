31120321234c13d31dd8c611b7fae0b0cf3fd535
AnttiVaaranen <antti.vaaranen@gmail.com>
2022-08-04 21:46:50 +0300

    lisätty crud-toiminnot tuotteille

--- TilausDbApp/Controllers/ProductsController.cs
+++ TilausDbApp/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using TilausDbApp.Models;
@@ -10,21 +12,79 @@ namespace TilausDbApp.Controllers
     public class ProductsController : Controller
     {
         // GET: Products
+        TilausDBEntities db = new TilausDBEntities();
+
         public ActionResult Index()
         {
-            TilausDBEntities db = new TilausDBEntities();
             List<Tuotteet> model = db.Tuotteet.ToList();
-            db.Dispose();
-
             return View(model);
         }
 
         public ActionResult ProductCard()
         {
-            TilausDBEntities db = new TilausDBEntities();
             List<Tuotteet> model = db.Tuotteet.ToList();
-            db.Dispose();
             return View(model);
+        }
+
+        
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Tuotteet tuote = db.Tuotteet.Find(id);
+            if (tuote == null) return HttpNotFound();
+            return View(tuote);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit([Bind(Include = "Nimi, Ahinta, Kuvalinkki")] Tuotteet tuote)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tuote).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ProductCard");
+            }
+            return View(tuote);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create([Bind(Include = "Nimi, Ahinta, Kuvalinkki")] Tuotteet tuote)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tuotteet.Add(tuote);
+                db.SaveChanges();
+                return RedirectToAction("ProductCard");
+            }
+            return View(tuote);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Tuotteet tuote = db.Tuotteet.Find(id);
+            if (tuote == null) return HttpNotFound();
+            return View(tuote);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tuotteet tuote = db.Tuotteet.Find(id);
+            db.Tuotteet.Remove(tuote);
+            db.SaveChanges();
+            return RedirectToAction("ProductCard");
         }
     }
 }
